3193b53b1ac1a5948aa42e41f0e17f8c0bc79f61
Esposito Antonio (BarP/LOI) <Antonio.Esposito@it.bosch.com>
2020-11-04 17:15:04 +0100

    First commit - Solution and base class

---
+++ ChatDemo/Controllers/ChatController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ChatDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private readonly ILogger<ChatController> _logger;
+
+        public ChatController(ILogger<ChatController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Message> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Message
+            {
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ ChatDemo/Message.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ChatDemo
+{
+    public class Message
+    {
+        public DateTime CreatedAt { get; set; }
+
+        public string Name { get; set; }
+
+        public string Text { get; set; }
+    }
+}
